90ba5c4705103d39242681d2bd816df371b5850c
Wojciech Koza <kriczq@gmail.com>
2018-11-14 18:15:11 +0100

    Added authentication listeners

---
+++ backend/src/main/java/pl/ustudni/portal/security/AuthenticationSuccessListener.java
@@ -0,0 +1,32 @@
+package pl.ustudni.portal.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+import pl.ustudni.portal.service.LoginAttemptService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class AuthenticationSuccessListener implements ApplicationListener<AuthenticationSuccessEvent> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Override
+    public void onApplicationEvent(final AuthenticationSuccessEvent e) {
+        System.out.println("Authentication success");
+
+        final String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null) {
+            loginAttemptService.loginSucceeded(request.getRemoteAddr());
+        } else {
+            loginAttemptService.loginSucceeded(xfHeader.split(",")[0]);
+        }
+    }
+
+}
